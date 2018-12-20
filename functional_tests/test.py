9fb46c69eef75ccfe080da117d29e12561146edc
wuxueyan_cd@keruyun.com <Kidding123456>
2018-12-20 18:07:17 +0800

    split

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -5,13 +5,12 @@ import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 class NewVisitorTest(StaticLiveServerTestCase):
-	# 类专属方法
-	# ，可通过类名进行调用
+	# 类专属方法可通过类名进行调用
 	@classmethod
 	def setUpClass(cls):
 		for arg in sys.argv:
 			if 'liveserver' in arg:
-				cls.server_url = 'http://' + arg.sqlit('=')[1]
+				cls.server_url = 'http://' + arg.split('=')[1]
 				return
 		super().setUpClass()
 		cls.server_url = cls.live_server_url
